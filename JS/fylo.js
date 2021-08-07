9bbfc2c44acba8e172912c62387d554a5de7b564
Emmanuel Okon <emmaudo27@gmail.com>
2021-08-07 16:33:25 +0100

    Initial commit containing all assets and files

---
+++ JS/fylo.js
@@ -0,0 +1,17 @@
+//alert("welcome back");
+
+// Define UI Variables
+const logo = document.querySelector(".logo");
+const navLinks = document.querySelector(".nav-links");
+const getStarted = document.querySelector(".getStarted");
+const email = document.querySelector("#email");
+
+// Load all event listeners
+loadEventListeners();
+
+
+function loadEventListeners() {
+    logo.addEventListener("mouseon", pointer);
+    
+}
+

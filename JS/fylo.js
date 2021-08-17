192349f7d14bb9e520d7a177d9c5f80dbcf9bea2
Emmanuel Okon <emmaudo27@gmail.com>
2021-08-17 19:47:07 +0100

    footer partials added and modifications on sass files

--- JS/fylo.js
+++ JS/fylo.js
@@ -1,4 +1,4 @@
-//alert("welcome back");
+// alert("welcome back");
 
 // Define UI Variables
 const logo = document.querySelector(".logo");
@@ -6,12 +6,12 @@ const navLinks = document.querySelector(".nav-links");
 const getStarted = document.querySelector(".getStarted");
 const email = document.querySelector("#email");
 
+
 // Load all event listeners
 loadEventListeners();
 
-
 function loadEventListeners() {
-    logo.addEventListener("mouseon", pointer);
-    
+  logo.addEventListener("mouseon", pointer);
 }
+
 

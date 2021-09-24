c5d2c523cbbc770946840f68cb4f00489bd2ded2
Emmanuel Okon <emmaudo27@gmail.com>
2021-09-24 17:42:29 +0100

    Animation

--- JS/fylo.js
+++ JS/fylo.js
@@ -1,17 +1,17 @@
-// alert("welcome back");
-
 // Define UI Variables
-const logo = document.querySelector(".logo");
-const navLinks = document.querySelector(".nav-links");
-const getStarted = document.querySelector(".getStarted");
-const email = document.querySelector("#email");
-
+// const logo = document.querySelector(".logo");
+// const navLinks = document.querySelector(".nav-links");
+// const getStarted = document.querySelector(".getStarted");
+// const email = document.querySelector("#email");
 
 // Load all event listeners
-loadEventListeners();
+// loadEventListeners();
 
-function loadEventListeners() {
-  logo.addEventListener("mouseon", pointer);
-}
+// function loadEventListeners() {
+//   logo.addEventListener("mouseon", pointer);
+// }
 
+
+const introImg = document.querySelector('.imageOne');
+introImg.classList.add('animate__animated', 'animate__bounceOutLeft');
 
